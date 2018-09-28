bbe8986577588bbc8ecc54813f2aab082cb1053f
larchrono <larchrono@hotmail.com>
2018-09-29 02:39:31 +0800

    BeReady

--- Assets/Scripts/CameraDynamicSetting.cs
+++ Assets/Scripts/CameraDynamicSetting.cs
@@ -4,50 +4,83 @@ using UnityEngine;
 
 public class CameraDynamicSetting : MonoBehaviour {
 
+	public string CamName;
+
+	string keyX;
+	string keyY;
+	string keyZ;
+	string keyRx;
+	string keyFov;
+
 	Camera cam;
+
 
 	void Awake(){
 		cam = GetComponent<Camera>();
 	}
 
+	public void ClearKey(){
+		PlayerPrefs.DeleteKey (keyX);
+		PlayerPrefs.DeleteKey (keyY);
+		PlayerPrefs.DeleteKey (keyZ);
+		PlayerPrefs.DeleteKey (keyRx);
+		PlayerPrefs.DeleteKey (keyFov);
+
+		Debug.Log ("Clear key:" + keyY);
+
+	}
+
 	// Use this for initialization
 	void Start () {
-		
 
-		if (!PlayerPrefs.HasKey ("C2Y")) {
+		keyX = CamName + "X";
+		keyY = CamName + "Y";
+		keyZ = CamName + "Z";
+		keyRx = CamName + "Rx";
+		keyFov = CamName + "Fov";
 
-			PlayerPrefs.SetFloat ("C2Y", cam.transform.localPosition.y);
-			PlayerPrefs.SetFloat ("C2Z", cam.transform.localPosition.z);
-			PlayerPrefs.SetFloat ("C2Rx", cam.transform.localRotation.eulerAngles.x);
-			PlayerPrefs.SetFloat ("C2Fov", cam.fieldOfView);
+		if (!PlayerPrefs.HasKey (keyX)) {
+
+			PlayerPrefs.SetFloat (keyX, cam.transform.localPosition.x);
+			PlayerPrefs.SetFloat (keyY, cam.transform.localPosition.y);
+			PlayerPrefs.SetFloat (keyZ, cam.transform.localPosition.z);
+			PlayerPrefs.SetFloat (keyRx, cam.transform.localRotation.eulerAngles.x);
+			PlayerPrefs.SetFloat (keyFov, cam.fieldOfView);
+
 		}
+	}
+
+	public void SliderX(float src){
+		Vector3 ov = cam.transform.localPosition;
+		cam.transform.localPosition = new Vector3 (src, ov.y, ov.z);
+		PlayerPrefs.SetFloat (keyX, src);
 	}
 
 	public void SliderY(float src){
 		Vector3 ov = cam.transform.localPosition;
 		cam.transform.localPosition = new Vector3 (ov.x, src, ov.z);
-		PlayerPrefs.SetFloat ("C2Y", src);
+		PlayerPrefs.SetFloat (keyY, src);
 	}
 
 	public void SliderZ(float src){
 		Vector3 ov = cam.transform.localPosition;
 		cam.transform.localPosition = new Vector3 (ov.x, ov.y, src);
-		PlayerPrefs.SetFloat ("C2Z", src);
+		PlayerPrefs.SetFloat (keyZ, src);
 	}
 
 	public void SliderRx(float src){
 		Vector3 ov = cam.transform.localRotation.eulerAngles;
 		cam.transform.localRotation = Quaternion.Euler (new Vector3 (src, ov.y, ov.z));
-		PlayerPrefs.SetFloat ("C2Rx", src);
+		PlayerPrefs.SetFloat (keyRx, src);
 	}
 
 	public void SliderFoV(float src){
 		cam.fieldOfView = src;
-		PlayerPrefs.SetFloat ("C2Fov", src);
+		PlayerPrefs.SetFloat (keyFov, src);
 	}
 
 	public void ShowAllValue(){
-		string msg = "y:" + cam.transform.localPosition.y + " , z:" + cam.transform.localPosition.z + " , Rx:" + cam.transform.localRotation.eulerAngles.x +
+		string msg = "x:" + cam.transform.localPosition.x + " , y:" + cam.transform.localPosition.y + " , z:" + cam.transform.localPosition.z + " , Rx:" + cam.transform.localRotation.eulerAngles.x +
 		             " , FoV:" + cam.fieldOfView;
 		DebugInterface.DebugMsg (msg);
 	}
--- Assets/Scripts/SliderGetDefault.cs
+++ Assets/Scripts/SliderGetDefault.cs
@@ -5,16 +5,24 @@ using UnityEngine.UI;
 
 public class SliderGetDefault : MonoBehaviour {
 
+	public CameraDynamicSetting camSetting;
 	public string valueKey;
 	Slider sld;
 
 	// Use this for initialization
 	void Start () {
 		sld = GetComponent<Slider> ();
-		GetPlayerValue (sld,valueKey);
+		valueKey = camSetting.CamName + valueKey;
+		StartCoroutine( GetPlayerValue (sld,valueKey));
 	}
+
+
 	
-	public void GetPlayerValue(Slider sld,string key){
+	IEnumerator GetPlayerValue(Slider sld,string key){
+		yield return new WaitForSeconds (0.5f);
+
+		if (!PlayerPrefs.HasKey (key))
+			yield break;
 		float val = PlayerPrefs.GetFloat (key, 0);
 		sld.value = val;
 	}
